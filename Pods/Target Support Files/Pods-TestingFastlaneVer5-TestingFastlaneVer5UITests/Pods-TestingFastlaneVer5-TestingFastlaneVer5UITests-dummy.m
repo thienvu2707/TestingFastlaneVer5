62e18a4ceff8aae2f545d3b69c5edaedb604e795
Thien Vu Le <vule@gumiviet.com>
2021-04-08 16:06:50 +0700

    feat: add Firebase and fastlane

---
+++ Pods/Target Support Files/Pods-TestingFastlaneVer5-TestingFastlaneVer5UITests/Pods-TestingFastlaneVer5-TestingFastlaneVer5UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestingFastlaneVer5_TestingFastlaneVer5UITests : NSObject
+@end
+@implementation PodsDummy_Pods_TestingFastlaneVer5_TestingFastlaneVer5UITests
+@end
---
+++ Pods/Target Support Files/Pods-TestingFastlaneVer5Tests/Pods-TestingFastlaneVer5Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestingFastlaneVer5Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TestingFastlaneVer5Tests
+@end
